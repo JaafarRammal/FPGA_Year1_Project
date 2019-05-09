ef10fdd7029c44bd8c77a32cacdb3efc1ddf377d
Jaafar Rammal <45538723+JaafarRammal@users.noreply.github.com>
2019-05-10 00:40:46 +0300

    Optimized
    Removed some of the inputs. Even though the latency doesn't change, the overall FPS will be better, since we have less inputs to communicate to the FPGA from the CPU for every frame

--- Full_Frame_CV2_720pi/source.cpp
+++ Full_Frame_CV2_720pi/source.cpp
@@ -4,39 +4,31 @@
 #include <assert.h>
 
 
-void flap_detector(volatile uint32_t* in_hand, volatile uint32_t* out_data, uint16_t* mean_height, bool* flap, bool* rise, uint8_t colour_threshold, uint8_t height_threshold){
-
+void flap_detector(volatile uint32_t* in_hand, uint16_t* mean_height, uint8_t colour_threshold){
 
 #pragma HLS INTERFACE s_axilite port=returns
-
-#pragma HLS INTERFACE m_axi depth=921600 port=in_hand offset=slave // This will NOT work for resolutions higher than 720p
-#pragma HLS INTERFACE m_axi depth=921600 port=out_data offset=slave
+#pragma HLS INTERFACE m_axi depth=921600 port=in_hand offset=slave
 
 	/*
 	 * Different dimensions:
 	 *
 	 * Format: W x H
 	 *
-	 * Output frame (out_data): 1280x720
-	 * HDMI_in (in_hand): 1280x720 (used region is top-left quarter 960x540)
+	 * HDMI_in (in_hand): 1280x720 (used region is top-left quarter 640x360)
 	 * Detected hand: 640x360
+	 * Output: 1280x720
 	 */
 
-	////////////////////////////////////////////////////
-	///////////////HAND POSITION DETECTION//////////////
-	////////////////////////////////////////////////////
-	int total_height = 0;
-	int pixel_number = 0;
-
-	int previous_mean_height = *mean_height;
+	unsigned int total_height = 0;
+	unsigned int pixel_number = 0;
 
 	loop:for(int i = 0; i < 460799; i++){
+
+			#pragma HLS PIPELINE II=2
 
 			if(i%1280 < 640){
 
 				unsigned int current = in_hand[i];
-
-				out_data[i] = current;
 
 				unsigned char in_r = current & 0xFF;
 				unsigned char in_g = (current >> 8) & 0xFF;
@@ -44,33 +36,20 @@ void flap_detector(volatile uint32_t* in_hand, volatile uint32_t* out_data, uint
 
 				if((in_g > (in_r + colour_threshold)) & (in_g > (in_b + colour_threshold))){
 
-					total_height += (720-i/1280); //i starts from the top, so the pixel height would be total height minus i;
+					total_height += i/1280;
 					pixel_number ++;
 
-					out_data[i+460799] = 0xFFFFFF;
+					in_hand[i+460799] = 0xFFFFFF;
 				}
 
 				else{
-					out_data[i+460799] = 0;
+					in_hand[i+460799] = 0;
 				}
 			}
 
 
 		}
+
 	*mean_height = total_height / pixel_number;
 
-	////////////////////////////////////////////////////
-	///////////////////FLAP DETECTION///////////////////
-	////////////////////////////////////////////////////
-
-	if((previous_mean_height > (*mean_height + height_threshold)) & (*rise == true)){
-
-		*flap = true;
-		*rise = false;
-	}
-
-	if((*mean_height > (previous_mean_height + height_threshold)) & (*rise == false)){
-
-		*rise = false;
-	}
 }
