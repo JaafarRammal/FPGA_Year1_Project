1a7082942ab070cce477d8c2b66f00581d6266cc
Jaafar Rammal <45538723+JaafarRammal@users.noreply.github.com>
2019-05-10 00:41:30 +0300

    Optimized

--- Full_Frame_CV2_720pi/tb.cpp
+++ Full_Frame_CV2_720pi/tb.cpp
@@ -18,12 +18,12 @@ struct pixel_data {
 	pixel_type red;
 };
 
-void flap_detector(volatile uint32_t* in_hand, volatile uint32_t* out_data, uint16_t* mean_height, bool* flap, bool* rise, uint8_t colour_threshold, uint8_t height_threshold);
+void flap_detector(volatile uint32_t* in_hand, uint16_t* mean_height, uint8_t colour_threshold);
 
 int main() {
 
 	//Hand input
-	cv::Mat hand_src = cv::imread("/home/jr4918/720_cv2/hand.jpg", CV_LOAD_IMAGE_UNCHANGED);
+	cv::Mat hand_src = cv::imread("/home/jr4918/720piCV2/test_files/in1.jpg", CV_LOAD_IMAGE_UNCHANGED);
 	std::cout << "Hand Image type: " << hand_src.type() << ", no. of channels: " << hand_src.channels() << std::endl;
 	std::cout<<" Hand Image size  "<<hand_src.size()<<std::endl;
 	uchar *hand_data = hand_src.data;
@@ -35,36 +35,28 @@ int main() {
 		hand_im[4*i + 3] = 0;
 	}
 
-
-
-	//Output
-	cv::Mat out_src = cv::imread("/home/jr4918/720_cv2/out.jpg", CV_LOAD_IMAGE_UNCHANGED);
-	std::cout << "Out Image type: " << out_src.type() << ", no. of channels: " << hand_src.channels() << std::endl;
-	std::cout<<" Out Image size  "<<out_src.size()<<std::endl;
-	uchar *out_data = out_src.data;
-	uchar *out_im = (uchar *)malloc(720*1280*4);
-
 	std::cout<<"Executing..."<<std::endl;
 
 	uint16_t mean_height =700;
 	bool flap = false;
 	bool rise = true;
 
-    flap_detector((volatile uint32_t *)hand_im, (volatile uint32_t *)out_im, &mean_height, &flap, &rise, 29, 50);
+    flap_detector((volatile uint32_t *)hand_im, &mean_height, 29);
 
     std::cout<<"Writing..."<<std::endl;
 	for (int i=0; i<1280*720; i++){
-		out_data[3*i + 2] = out_im[4*i + 0];
-		out_data[3*i + 1] = out_im[4*i + 1];
-		out_data[3*i + 0] = out_im[4*i + 2];
+		hand_data[3*i + 2] = hand_im[4*i + 0];
+		hand_data[3*i + 1] = hand_im[4*i + 1];
+		hand_data[3*i + 0] = hand_im[4*i + 2];
 	}
-	std::cout<<"Success!"<<std::endl;
+	std::cout<<"Success! Exporting..."<<std::endl;
 
 	//Specify an Absolute Path for Storing Output Image
-	cv::imwrite("/home/jr4918/hd_test/final.jpg",out_src);
+	cv::imwrite("/home/jr4918/720piCV2/test_files/out1.jpg",hand_src);
 
 	free(hand_im);
-	free(out_im);
+
+	std::cout<<"Done!"<<std::endl;
 
 	return 0;
 
