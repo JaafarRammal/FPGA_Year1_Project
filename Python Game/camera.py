6c0de3a5555b433bfeeb39ebe971505d2ecdd878
Jaafar Rammal <45538723+JaafarRammal@users.noreply.github.com>
2019-05-10 19:58:55 +0100

    Create camera.py

---
+++ Python Game/camera.py
@@ -0,0 +1,23 @@
+import cv2
+
+def show_webcam(mirror=False):
+    cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+    while True:
+        ret_val, img = cam.read()
+        if mirror: 
+            img = cv2.flip(img, 1)
+        cv2.imshow('my webcam', img)
+        cv2.setWindowProperty('my webcam',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+        if cv2.waitKey(1) == 27: 
+            break  # esc to quit
+    cv2.destroyAllWindows()
+
+
+def main():
+    show_webcam(mirror=True)
+
+
+if __name__ == '__main__':
+    main()
